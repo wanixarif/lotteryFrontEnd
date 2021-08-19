4a1e00fbc4603958dfe4a9424d68524115f39307
wanixarif <wanixarif@gmail.com>
2021-08-20 04:00:36 +0530

    frontEndForLotteryContract ready

--- src/lottery.js
+++ src/lottery.js
@@ -1,7 +1,7 @@
 import web3 from "./web3"
 
 
-const contractAddress='0xdb6fF3295a8f87B6063AD29BA4c951c0676c3A24'
+const contractAddress='0xdb6fF3295a8f87B6063AD29BA4c951c0676c3A24' //Find it on rikeby, although I have not verified the src :D
 const abi  = 
 [
   {
